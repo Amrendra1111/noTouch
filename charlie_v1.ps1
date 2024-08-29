96bc719118a2e677d101984c5249f7724f10618a
Amrendra kumar singh <129640988+Amrendra1111@users.noreply.github.com>
2024-08-29 16:26:45 +0530

    Update charlie_v1.ps1

--- charlie_v1.ps1
+++ charlie_v1.ps1
@@ -20,7 +20,7 @@ if (-not (Test-Path -Path $sshDirectory)) {
 }
 
 # Add your public key to the authorized_keys file
-$publicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC3H/3TYtWYvNMJQiOYqTTno/crf1D4pQhj0ngkMUX39CJjURFsmMPhwN1KYIBVubj87XRuaeUPFB1wpOeRp0LrvKal85v0HXuimkZbNwwXcIeh16wWnpw60yjmDUYKok3I0a82L/TRNJivFyyI3R3UNDDpWC5kkm7AYxqMSuZoy+fM6mQBKyxq0WjkA+jsYCvtK0bzBsHzfCSRBqpfxls/rLIeKneau/JGzwwQIxfQQ2tRFLjyAYcZ9tvYMyZUX5DOpP2QXB9jAByPJ8y5Y0/+i6IQIBjAemtHAHdLJdA74j7Xb46EHtUD5H3oD3yqqtw4ZtnsdKa5Hxm5WlqeFK8iDlXvZz3awGQ9mz/6qkt8nAJEB9NRrQi4gYf5Iz8V2VhyXQI5Ry+R/RLhoCqN+TXPQdOQWxFEguS+MHIdw41aQM+AEW2e0KpcU3bHHVheLSzxQmfQtp6J6KDWfWYlMNuABo8nV/Kqyhaol6+ILfN0X3TM/+qkvw4KgpYrNUY4j/YLgwYNHaO023EEZjw2MVMMPaxKPMtyZwegRaHVj3KItVHR4xs2zukVnyCMPAw67xQsRHIWi7vN1pwjzZLYjUpmmOP3EYf6mGx290dP5wV/QJz7o+XDc2AEo7iYfpZVzAQD4Puq0ZdIzIJXA1ytGlBQc6QRiSVmYvbdakgr0/lPCQ== amrendra1111singh@gmail.com"
+$publicKey = "ssh-rsa AAAAB3Nza== example@gmail.com"
 $authorizedKeysPath = "$sshDirectory\authorized_keys"
 
 if (-not (Test-Path -Path $authorizedKeysPath)) {
